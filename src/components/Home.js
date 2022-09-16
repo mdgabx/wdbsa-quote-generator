023fc0d2b94b27460524330d7dd87d0a1355a022
Mark Abuyabor <mark@zeguestlist.com>
2022-09-16 08:16:29 +0800

    change to api to just plain JSON object

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,33 +1,30 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 import TwitterIcon from '@mui/icons-material/Twitter';
 import { connect } from 'react-redux';
 
+class Home extends Component {
 
-class Home extends Component{
-   
     handleClick = () => {
-        console.log('props', this.props);
         const { quotes } = this.props;
         const randomIndex = Math.floor(Math.random() * quotes.length);
         
-    this.setState({
-            quotes: this.props.quotes[randomIndex]
-    })
+        this.setState({
+                quotes: this.props.quotes[randomIndex]
+        })
     };
 
     render() {
-        console.log('props', this.props);
-
+        console.log('props in Home', this.props)
         const { quotes } = this.props;
         const randomIndex = Math.floor(Math.random() * quotes.length);
 
         return (
             <div className="container-fluid home d-flex">
-                <div className="col">
+                  <div className="col">
                     <h1 className="text-center py-5">Random Quotes Generator</h1>
                     <div className="container py-4 d-flex mx-auto align-items-center justify-content-center w-50" id="quote-box" >
                         <div className="col mx-auto justify-content-center">
-                            <p id="text" className="text-center">" { quotes[randomIndex]["quote"] } "</p>
+                            <p id="text" className="text-center">" { quotes[randomIndex]["quote"]  } "</p>
                             <hr />
                             <p id="author">- { quotes[randomIndex]["author"] }</p>
                             <div className="row group-btn mt-2 py-2">
@@ -36,14 +33,14 @@ class Home extends Component{
                                 >
                                 New Quote
                                 </button>
-                                <a className="btn btn-primary w-20" href="/twitter.com/intent/tweet" alt="twitter quote" target="_blank" id="tweet-quote">
+                                <a className="btn btn-primary w-20" href="https://twitter.com/twitter.com/intent/tweet" alt="twitter quote" target="_blank" id="tweet-quote">
                                     <TwitterIcon />
                                </a>
                             </div>         
                         </div>
                     </div> 
-                </div>
-            </div>
+                </div> 
+            </div> 
         )
     }
 }
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,24 +1,102 @@
 let initState = {
-    quotes: []
+    quotes: [
+        {
+          "id": 0,
+          "quote": "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+          "author": "Martin Fowler"
+        },
+        {
+          "id": 1,
+          "quote": "First, solve the problem. Then, write the code.",
+          "author": "John Johnson"
+        },
+        {
+          "id": 2,
+          "quote": "Experience is the name everyone gives to their mistakes.",
+          "author": "Oscar Wilde"
+        },
+        {
+          "id": 3,
+          "quote": "In order to be irreplaceable, one must always be different",
+          "author": "Coco Chanel"
+        },
+        {
+          "id": 4,
+          "quote": "Java is to JavaScript what car is to Carpet.",
+          "author": "Chris Heilmann"
+        },
+        {
+          "id": 5,
+          "quote": "Knowledge is power.",
+          "author": "Francis Bacon"
+        },
+        {
+          "id": 6,
+          "quote": "Sometimes it pays to stay in bed on Monday, rather than spending the rest of the week debugging Monday’s code.",
+          "author": "Dan Salomon"
+        },
+        {
+          "id": 7,
+          "quote": "Perfection is achieved not when there is nothing more to add, but rather when there is nothing more to take away.",
+          "author": "Antoine de Saint-Exupery"
+        },
+        {
+          "id": 8,
+          "quote": "Code is like humor. When you have to explain it, it’s bad.",
+          "author": "Cory House"
+        },
+        {
+          "id": 9,
+          "quote": "Fix the cause, not the symptom.",
+          "author": "Steve Maguire"
+        },
+        {
+          "id": 10,
+          "quote": "Optimism is an occupational hazard of programming: feedback is the treatment.",
+          "author": "Kent Beck"
+        },
+        {
+          "id": 11,
+          "quote": "When to use iterative development? You should use iterative development only on projects that you want to succeed.",
+          "author": "Martin Fowler"
+        },
+        {
+          "id": 12,
+          "quote": "Simplicity is the soul of efficiency.",
+          "author": "Austin Freeman"
+        },
+        {
+          "id": 13,
+          "quote": "Before software can be reusable it first has to be usable.",
+          "author": "Ralph Johnson"
+        },
+        {
+          "id": 14,
+          "quote": "Make it work, make it right, make it fast",
+          "author": "Kent Beck"
+        }
+      ]
 }
 
 /* fetch the data from the quotes mock api */
-const fetchQuotes = async () => {
+/*const fetchQuotes = async () => {
     const response = await fetch('https://wdbsa-quotes-api.herokuapp.com/quotes');
 
     if(response.status !== 200) {
         throw new Error('cannot fetch data');
     }
 
-    const data = await response.text();
+    const data = await response.json();
     return data;
 }
 
-    fetchQuotes().then(d => {
-           /* console.log('data', JSON.parse(d)); */
-            initState["quotes"] = JSON.parse(d);
+    fetchQuotes().then(response => {
+         console.log('response', response);
+         newArr = response;
+        return newArr;
+    }).catch(err => console.log(err));
 
-        }).catch(err => console.log(err));
+    console.log('newArr', newArr); */    
 
 const rootReducer = (state = initState, action) => {
     return state;
